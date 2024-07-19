db26062da897718fe0a493ea5812aaf0b4284d9a
Sven Schirmer <sven.glueckspilz@mastersong.de>
2024-07-19 08:08:55 +0200

    finish musterlösung

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import './App.css';
 import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 import GameSession from './pages/GameSession';
 import Rules from './pages/Rules';
 import Impressum from './pages/Impressum';
+import GlobalNavigation from './components/GlobalNavigation';
 
 function App() {
 
   return (
     <Routes>
-        <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="quiz" element={<GameSession />} />
-            <Route path="rules" element={<Rules />} />
-            <Route path="impressum" element={<Impressum />} />
-
-        </Route>
+      <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="quiz" element={<GameSession />} />
+          <Route path="rules" element={<Rules />} />
+          <Route path="impressum" element={<Impressum />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
@@ -22,6 +25,7 @@ function App() {
 function Layout() {
   return (
       <div className="App">
+      <GlobalNavigation />
       <div className="content">
           <header className="App-header">
           <h1>Welcome to the Wiss-Quiz</h1>
