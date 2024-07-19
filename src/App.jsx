55eaa54c39758635e32eb599e38e56184b319f95
Sven Schirmer <sven.glueckspilz@mastersong.de>
2024-07-19 07:49:30 +0200

    added BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import GameSession from './pages/GameSession';
 import Rules from './pages/Rules';
 import Impressum from './pages/Impressum';
@@ -6,17 +7,31 @@ import Impressum from './pages/Impressum';
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Willkommen beim WISS-Quiz</h1>
-        <hr />
-      </header>
-      <GameSession />
-      <hr />
-      <Rules />
-      <Impressum />
-    </div>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="quiz" element={<GameSession />} />
+            <Route path="rules" element={<Rules />} />
+            <Route path="impressum" element={<Impressum />} />
+
+        </Route>
+    </Routes>
   );
 }
+
+function Layout() {
+  return (
+      <div className="App">
+      <div className="content">
+          <header className="App-header">
+          <h1>Welcome to the Wiss-Quiz</h1>
+          <hr />
+          <Outlet />
+          <hr />
+          </header>
+      </div>
+      </div>
+  );
+  }
 
 export default App;
