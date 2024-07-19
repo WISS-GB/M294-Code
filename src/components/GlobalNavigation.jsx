cd187847aa71abf482a9b2552db538df4b729d8b
Sven Schirmer <sven.glueckspilz@mastersong.de>
2024-07-19 08:09:21 +0200

    finish musterlösung

---
+++ src/components/GlobalNavigation.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export default function GlobalNavigation() {
+  return (
+    <nav className='GlobalNavigation'>
+      <Link to="/">Home</Link>
+      <Link to="/quiz">Quiz</Link>
+      <Link to="/rules">Rules</Link>
+      <Link to="/impressum">Impressum</Link>
+    </nav>
+  );
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>This URL no have :P</h1>
+    </div>
+  );
+}
