5419f07fe742b36b8db186383e3f3ab027aa0a6a
Sven Schirmer <sven.glueckspilz@mastersong.de>
2024-07-28 10:31:15 +0200

    fixed previous tests

--- src/components/Question.test.js
+++ src/components/Question.test.js
@@ -5,14 +5,14 @@ import Question from './Question';
 describe("Dies ist ein Testpaket für die Question-Komponente", () => (
   test('renders Question component', () => {
     // Arrange
-    const q = {
+    const q = [{
       "question": "Who am I",
       "answers" : ["Answer 1","Answer 2","Answer 3"],
       "correct_answer": "Answer 1"
-    };
+    }];
     // Act
     render(
-      <Question key="123" question= { q }/>
+      <Question key="123" questions = { q }/>
     );
 
     // Assert
--- src/pages/GameSession.jsx
+++ src/pages/GameSession.jsx
@@ -3,7 +3,6 @@ import Question from "../components/Question";
 
 export default function GameSession(props) {
 
-
   const [questions, getQuestions] = useState(props.questions!==undefined? props.questions : [{
     question: "What is the answer to life, the universe and everything?",
     answers: ["Answer 1", "3", "42"],
@@ -13,7 +12,7 @@ export default function GameSession(props) {
   return (
     <div>
       <h1>GameSession</h1>
-      <Question question={ questions[0] } />
+      <Question questions={ questions } />
     </div>
   );
 }
