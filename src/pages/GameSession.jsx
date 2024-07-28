38e766f8de089af0e0aaa31336a953a2e0eb2976
Sven Schirmer <sven.glueckspilz@mastersong.de>
2024-07-28 10:37:34 +0200

    updates demo questions

--- src/pages/GameSession.jsx
+++ src/pages/GameSession.jsx
@@ -6,7 +6,18 @@ export default function GameSession(props) {
   const [questions, getQuestions] = useState(props.questions!==undefined? props.questions : [{
     question: "What is the answer to life, the universe and everything?",
     answers: ["Answer 1", "3", "42"],
-  }]);
+    correct_answer: "42"
+  }, {
+    question: "What is the longest river in the world?",
+    answers: ["Nile", "Amazonas", "Yangtze"],
+    correct_answer: "Nile"
+  }, {
+    question: "What is the capital of Germany?",
+    answers: ["Berlin", "Hamburg", "Munich"],
+    correct_answer: "Berlin"
+  }
+
+]);
 
 
   return (
