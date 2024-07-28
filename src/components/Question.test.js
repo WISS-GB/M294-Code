0883a25ba1090734556193e22c3b9974172fe753
Sven Schirmer <sven.glueckspilz@mastersong.de>
2024-07-28 14:58:49 +0200

    created automated tests for quiz

--- src/components/Question.test.js
+++ src/components/Question.test.js
@@ -1,8 +1,8 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Question from './Question';
 
-describe("Dies ist ein Testpaket für die Question-Komponente", () => (
+describe("Questions testing", () => {
   test('renders Question component', () => {
     // Arrange
     const q = [{
@@ -25,4 +25,71 @@ describe("Dies ist ein Testpaket für die Question-Komponente", () => (
     let a3 = screen.getByText("Answer 3");
     expect(a3).toBeInTheDocument();
   })
-))
+
+  test("evaluate correct answer works", () => {
+    // Arrange
+    const q = [{
+      "question": "Who am I",
+      "answers" : ["Answer 1","Answer 2","Answer 3"],
+      "correct_answer": "Answer 1"
+    }];
+
+    // Act
+    render(
+      <Question key="123" questions = { q }/>
+    );
+    let a1 = screen.getByText("Answer 1");
+    fireEvent.click(a1);
+
+    // Assert
+    let feedback = screen.getByText(/score:.1.von.1/i);
+    expect(feedback).toBeInTheDocument();
+    let next = screen.getByText(/weiter/i);
+    expect(next).toBeInTheDocument();
+  })
+
+  test("evaluate wrong answer works", () => {
+    // Arrange
+    const q = [{
+      "question": "Who am I",
+      "answers" : ["Answer 1","Answer 2","Answer 3"],
+      "correct_answer": "Answer 1"
+    }];
+
+    // Act
+    render(
+      <Question key="123" questions = { q }/>
+    );
+    let a2 = screen.getByText("Answer 2");
+    fireEvent.click(a2);
+
+    // Assert
+    let feedback = screen.getByText(/score:.0.von.1/i);
+    expect(feedback).toBeInTheDocument();
+    let next = screen.getByText("Weiter");
+    expect(next).toBeInTheDocument();
+  })
+
+  test("evaluate only single answer possible", () => {
+    // Arrange
+    const q = [{
+      "question": "Who am I",
+      "answers" : ["Answer 1","Answer 2","Answer 3"],
+      "correct_answer": "Answer 1"
+    }];
+
+    // Act
+    render(
+      <Question key="123" questions = { q }/>
+    );
+    let a2 = screen.getByText("Answer 2");
+    fireEvent.click(a2);
+
+    let a3 = screen.getByText("Answer 1");
+    fireEvent.click(a3);
+
+    // Assert
+    let feedback = screen.getByText(/score:.0.von.1/i);
+    expect(feedback).toBeInTheDocument();
+  })
+})
